866ec99bdab227441383768005014dfc8c1a9d14
NikithaDevasoth <devasothnikitha09@gmail.com>
2024-12-05 22:49:17 +0530

    Initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+class sum{
+    public static void main(String args[]){
+        int a=12;
+        int b=89;
+        int sum=a+b;
+        System.out.println("The sum of " +a "and "+b "is" +sum);
+    }
+}
